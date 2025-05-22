7a8b193dc7461b611d0c0801a348b59ef3712388
SavelievGleb2003 <savelievglib2003@gmail.com>
2025-05-22 09:15:12 +0300

    add schema

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from posts.views import PostList, PostDetail
+from posts.views import PostList, PostDetail, UserDetail, UserList
 
 urlpatterns = [
+    path('users/', UserList.as_view(), name='user_list'),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user_datail'),
     path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
     path('', PostList.as_view(), name='post_list')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework import permissions
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django.contrib.auth import get_user_model
 from posts.models import Post
+from rest_framework.permissions import IsAdminUser
 from posts.permissions import IsAuthorOrReaOnly
-from posts.serializers import PostSerializer
+from posts.serializers import PostSerializer, UserSerializer
 # Create your views here.
 
 
@@ -18,3 +20,17 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReaOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+
+class UserList(ListCreateAPIView):
+    permission_classes = [IsAdminUser]
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+
+class UserDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminUser]
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
